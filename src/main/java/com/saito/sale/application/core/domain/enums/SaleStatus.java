6ff5883bb235769c8df57a44101519977c267731
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-06 20:11:03 -0300

    [feat] add sale domain class

--- src/main/java/com/saito/sale/application/core/domain/enums/SaleStatus.java
+++ src/main/java/com/saito/sale/application/core/domain/enums/SaleStatus.java
@@ -1,4 +1,27 @@
 package com.saito.sale.application.core.domain.enums;
 
+import java.util.Arrays;
+
 public enum SaleStatus {
+
+    PENDING(1),
+    FINALIZED(2),
+    CANCELED(3);
+
+    private final Integer statusId;
+
+    SaleStatus(Integer statusId){
+        this.statusId = statusId;
+    }
+
+    public Integer getStatusId() {
+        return statusId;
+    }
+
+    public static SaleStatus toEnum(Integer anId){
+        if(anId == null) return null;
+        return Arrays.stream(SaleStatus.values()).filter(status -> anId.equals(status.getStatusId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("status Id is invalid" + anId));
+    }
 }

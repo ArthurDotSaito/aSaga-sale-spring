6ea61f96d186f93babf714f3c73d08510d2177a0
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-07 00:07:47 -0300

    [chore] add KafkaTeamplate config

--- src/main/java/com/saito/sale/config/kafka/CustomSerializer.java
+++ src/main/java/com/saito/sale/config/kafka/CustomSerializer.java
@@ -1,4 +1,24 @@
 package com.saito.sale.config.kafka;
 
-public class CustomSerializer {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.saito.sale.adapters.out.message.SaleMessage;
+import org.apache.kafka.common.serialization.Serializer;
+import org.yaml.snakeyaml.serializer.SerializerException;
+
+
+public class CustomSerializer implements Serializer<SaleMessage> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public byte[] serialize(String s, SaleMessage message){
+        try{
+            if(message == null){
+                return null;
+            }
+            return objectMapper.writeValueAsBytes(message);
+        }catch (Exception e){
+            throw new SerializerException("Error when serializing SaleMessage to byte[]");
+        }
+    }
 }

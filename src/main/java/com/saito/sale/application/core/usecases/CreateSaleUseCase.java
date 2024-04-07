f8e5402beb5221d8192f9b8cec6bdd3d1bcfe198
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-06 23:12:22 -0300

    [feat] implementing CreateSaleUSeCase

--- src/main/java/com/saito/sale/application/core/usecases/CreateSaleUseCase.java
+++ src/main/java/com/saito/sale/application/core/usecases/CreateSaleUseCase.java
@@ -1,20 +1,29 @@
 package com.saito.sale.application.core.usecases;
 
 import com.saito.sale.application.core.domain.Sale;
+import com.saito.sale.application.core.domain.enums.SaleEvent;
 import com.saito.sale.application.core.domain.enums.SaleStatus;
 import com.saito.sale.application.ports.out.SaveSaleOutputPort;
+import com.saito.sale.application.ports.out.SendCreatedSaleOutputPort;
 
 public class CreateSaleUseCase {
 
     private final SaveSaleOutputPort saveSaleOutputPort;
 
-    public CreateSaleUseCase(SaveSaleOutputPort saveSaleOutputPort){
+    private final SendCreatedSaleOutputPort sendCreatedSaleOutputPort;
+
+    public CreateSaleUseCase(SaveSaleOutputPort saveSaleOutputPort,
+                             SendCreatedSaleOutputPort sendCreatedSaleOutputPort
+    )
+    {
         this.saveSaleOutputPort = saveSaleOutputPort;
+        this.sendCreatedSaleOutputPort = sendCreatedSaleOutputPort;
     }
 
     public void create(Sale aSale){
         aSale.setStatus(SaleStatus.PENDING);
 
         var saleResponse = saveSaleOutputPort.save(aSale);
+        sendCreatedSaleOutputPort.send(saleResponse, SaleEvent.CREATED_SALE);
     }
 }
--- src/main/java/com/saito/sale/application/ports/out/SendCreatedSaleOutputPort.java
+++ src/main/java/com/saito/sale/application/ports/out/SendCreatedSaleOutputPort.java
@@ -1,9 +1,9 @@
 package com.saito.sale.application.ports.out;
 
 import com.saito.sale.application.core.domain.Sale;
+import com.saito.sale.application.core.domain.enums.SaleEvent;
 
 public interface SendCreatedSaleOutputPort {
 
     void send(Sale aSale, SaleEvent aEvent);
-
 }
